30c8d7f5090176d29599d9b312fdcdbe9af6341d
Alexandre Charoy <alexandre.charoy@gmail.com>
2018-12-14 01:27:26 +0100

    Added some tests

---
+++ java_src/DeadCodeElimination/src/DeadCodeElimination.java
@@ -0,0 +1,11 @@
+
+public class DeadCodeElimination {
+
+	public static void main(String[] args) {
+		
+		return;
+		int x = 0;
+
+	}
+
+}
---
+++ java_src/LoopUnrolling/src/LoopUnrolling.java
@@ -0,0 +1,15 @@
+
+public class LoopUnrolling {
+
+	public static void main(String[] args) {
+		
+		int max = 3;
+		int result = 0;
+		for (int i = 0; i < max; i++)
+		{
+			result++;
+		}
+
+	}
+
+}
